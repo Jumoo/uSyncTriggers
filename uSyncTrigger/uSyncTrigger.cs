605fb95ecf2346d26cf65e6cb58b3e0161122caf
Kevin Jump <kevin@thejumps.co.uk>
2021-07-31 12:08:11 +0100

    Move urls to constants.

---
+++ uSyncTrigger/uSyncTrigger.cs
@@ -0,0 +1,10 @@
+﻿namespace uSyncTrigger
+{
+    public static class uSyncTrigger
+    {
+        private const string _triggerBaseUrl = "uSync/trigger/";
+
+        public static string ImportUrl = _triggerBaseUrl + "import";
+        public static string ExportUrl = _triggerBaseUrl + "export";
+    }
+}
