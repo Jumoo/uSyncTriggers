0b5de009f63f4b6cdd237374f83f82dee9915760
Kevin Jump <kevin@thejumps.co.uk>
2022-07-05 12:13:10 +0100

    Add v10 site.

--- uSyncTrigger.Site/Program.cs
+++ uSyncTrigger.Site/Program.cs
@@ -1,8 +1,3 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
 namespace uSyncTrigger.Site
 {
     public class Program
@@ -14,7 +9,11 @@ namespace uSyncTrigger.Site
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(x => x.ClearProviders())
-                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+                .ConfigureUmbracoDefaults()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStaticWebAssets();
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
